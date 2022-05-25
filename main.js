e9dee59f309fb74c9f75491cef8d45e21e39f640
Mai Duc Thang <thangmdfx12498@funix.edu.vn>
2022-05-25 11:57:23 +0700

    Add render part

--- main.js
+++ main.js
@@ -1,2 +1,124 @@
-$(document).ready(function() {
+const songs = [
+    {
+        name: 'Viva La Vida',
+        singer: 'J.Fla Cover',
+        path: './assets/music/Coldplay - Viva La Vida ( cover by J.Fla ).mp3',
+        image: './assets/img/song1.jpg'
+    },
+
+    {
+        name: 'Sold Out',
+        singer: 'Hawk Nelson',
+        path: './assets/music/Hawk Nelson - Sold Out (Official Lyric Video).mp3',
+        image: './assets/img/sold out.jpg'
+    },
+
+    {
+        name: 'Arcade',
+        singer: 'Duncan Laurence',
+        path: './assets/music/Arcade - Duncan Laurence {LYRICS} -!WINNER ESC 2019!-.mp3',
+        image: './assets/img/arcade.jpg'
+    },
+
+    {
+        name: 'Send my love to your new lover',
+        singer: 'Sit Still, Look Pretty',
+        path: './assets/music/Send my love to your new lover - sit still, look pretty -- Vietsub.mp3',
+        image: './assets/img/send my love.jpg'
+    },
+
+    {
+        name: 'Waiting For Love',
+        singer: 'Romy Wave Cover',
+        path: './assets/music/Waiting For Love - Romy Wave Cover - Phong Max Remix (Lyrics + Vietsub) ♫ (1).mp3',
+        image: './assets/img/waiting.jpg'
+    },
+
+    {
+        name: 'Hương lúa',
+        singer: 'Jay Chou',
+        path: './assets/music/Fragrant Rice - Hương Lúa [Vietsub + Kara] [HD].mp3',
+        image: './assets/img/jaychou.jpg'
+    }
+]
+
+// 1. Render song
+// 2. Scroll top
+// 3. Play/pause/seek
+// 4. CD run 
+// 5. Next/prev 
+// 6. Random 
+// 7. Next/repeat 
+// 8. Active song 
+// 9. Scroll active song into view 
+// 10. Play song when click 
+$(document).ready(function() {  
+    const app = {
+        songs : [
+            {
+                name: 'Viva La Vida',
+                singer: 'J.Fla Cover',
+                path: './assets/music/Coldplay - Viva La Vida ( cover by J.Fla ).mp3',
+                image: './assets/img/song1.jpg'
+            },
+        
+            {
+                name: 'Sold Out',
+                singer: 'Hawk Nelson',
+                path: './assets/music/Hawk Nelson - Sold Out (Official Lyric Video).mp3',
+                image: './assets/img/sold out.jpg'
+            },
+        
+            {
+                name: 'Arcade',
+                singer: 'Duncan Laurence',
+                path: './assets/music/Arcade - Duncan Laurence {LYRICS} -!WINNER ESC 2019!-.mp3',
+                image: './assets/img/arcade.jpg'
+            },
+        
+            {
+                name: 'Send my love to your new lover',
+                singer: 'Sit Still, Look Pretty',
+                path: './assets/music/Send my love to your new lover - sit still, look pretty -- Vietsub.mp3',
+                image: './assets/img/send my love.jpg'
+            },
+        
+            {
+                name: 'Waiting For Love',
+                singer: 'Romy Wave Cover',
+                path: './assets/music/Waiting For Love - Romy Wave Cover - Phong Max Remix (Lyrics + Vietsub) ♫ (1).mp3',
+                image: './assets/img/song1.jpg'
+            },
+        
+            {
+                name: 'Hương lúa',
+                singer: 'Jay Chou',
+                path: './assets/music/Fragrant Rice - Hương Lúa [Vietsub + Kara] [HD].mp3',
+                image: './assets/img/jaychou.jpg'
+            }
+        ],
+        render: function() {
+            const htmls = this.songs.map(song => {
+                return ` 
+                    <div class="song">
+                        <div class="thumb" style="background-image: url('${song.image}')">
+                        </div>
+                        <div class="body">
+                            <h3 class="title">${song.name}</h3>
+                            <p class="author">${song.singer}</p>
+                        </div>
+                        <div class="option">
+                            <i class="fa-solid fa-ellipsis"></i>
+                        </div>
+                    </div>`
+            })
+            console.log(htmls);
+            $('.playlist').html(htmls.join('')) ;
+        },
+        
+        start: function() {
+            this.render();
+        }
+    }
+    app.start()
 });
